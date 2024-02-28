07b2d1af58badcd0f266c45585690c1cdc01de02
Soumaditya Pal <soumaditya.pal23@gmail.com>
2024-02-28 23:39:14 +0530

    Delete 1. Week 1 C++ Assignment 1 directory

--- 1. Week 1 C++ Assignment 1/print.cpp
+++
@@ -1,7 +0,0 @@
-// How can you output “Physics” and “Wallah” in two different lines in C++?
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"Physics"<<endl<<"Wallah";
-}
--- 1. Week 1 C++ Assignment 1/variable_name.cpp
+++
@@ -1,6 +0,0 @@
-// How many of these can be a variable name ?
-// 01Pwskills-no
-// _FLOAT-yes
-// int-no
-// FLOAT-yes
-// You will succeed-no
