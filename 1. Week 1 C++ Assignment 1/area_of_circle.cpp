f06d6a4a95f49d30595ef684ae3d9c9f8818d428
Soumaditya Pal <soumaditya.pal23@gmail.com>
2024-02-28 23:16:55 +0530

    Add files via upload

---
+++ 1. Week 1 C++ Assignment 1/area_of_circle.cpp
@@ -0,0 +1,11 @@
+
+// Write a program to find the circumference of a circle with radius 10 in C++.
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius =10;
+    float pi=3.1415;
+    float area_of_circle=pi*radius*radius;
+    cout<<"Area of a circle is : "<<area_of_circle;
+}
---
+++ 1. Week 1 C++ Assignment 1/datatype_size.cpp
@@ -0,0 +1,10 @@
+// How much space does the following data types take?
+// int
+// bool
+// float
+
+
+
+// int 2 byte or 4 byte
+// bool  1 byte
+// float 4 byte
